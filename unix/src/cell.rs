538ae0552197febd7de6aa05d03cf56e90945f1a
Stephen Sherratt <sfsherratt@gmail.com>
2018-01-07 12:28:17 +0000

    Better mapping of rgb24 to greyscale ansi colours

--- unix/src/cell.rs
+++ unix/src/cell.rs
@@ -8,9 +8,15 @@ pub struct Colour(pub ansi_colour::Colour);
 
 impl From<Rgb24> for Colour {
     fn from(rgb24: Rgb24) -> Self {
-        let red = rgb24.red as u32 * ansi_colour::MAX_RGB_CHANNEL as u32 / 255;
-        let green = rgb24.green as u32 * ansi_colour::MAX_RGB_CHANNEL as u32 / 255;
-        let blue = rgb24.blue as u32 * ansi_colour::MAX_RGB_CHANNEL as u32 / 255;
+
+        if rgb24.red == rgb24.green && rgb24.green == rgb24.blue {
+            let level = (rgb24.red as u32 * ansi_colour::MAX_GREY_SCALE as u32) / 255;
+            return Colour(ansi_colour::Colour::grey_scale(level as u8).unwrap());
+        }
+
+        let red = (rgb24.red as u32 * ansi_colour::MAX_RGB_CHANNEL as u32) / 255;
+        let green = (rgb24.green as u32 * ansi_colour::MAX_RGB_CHANNEL as u32) / 255;
+        let blue = (rgb24.blue as u32 * ansi_colour::MAX_RGB_CHANNEL as u32) / 255;
 
         Colour(ansi_colour::Colour::rgb(red as u8, green as u8, blue as u8).unwrap())
     }
