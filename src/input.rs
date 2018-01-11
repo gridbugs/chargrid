87a5f005ba53067765c5ea71822001d933b19fe5
Stephen Sherratt <sfsherratt@gmail.com>
2018-01-11 23:24:56 +0000

    More input aliases

--- src/input.rs
+++ src/input.rs
@@ -19,5 +19,7 @@ pub mod inputs {
 
     pub const ESCAPE: Input = Input::Char('\u{1b}');
     pub const ETX: Input = Input::Char('\u{3}');
+    pub const BACKSPACE: Input = Input::Char('\u{8}');
+    pub const TAB: Input = Input::Char('\u{9}');
     pub const RETURN: Input = Input::Char('\u{d}');
 }
