008821fb34af030a6ad1d693f817171f2264a91c
Stephen Sherratt <sfsherratt@gmail.com>
2018-01-13 16:04:08 +0000

    Add clean script

---
+++ clean.sh
@@ -0,0 +1,5 @@
+cargo clean --manifest-path Cargo.toml
+cargo clean --manifest-path prototty_app/Cargo.toml
+cargo clean --manifest-path unix_app/Cargo.toml
+cargo clean --manifest-path wasm_app/Cargo.toml
+cargo clean --manifest-path glutin_app/Cargo.toml
