899eefbbfb1dabae9fc290419ad66f59e128e7a8
Stephen Sherratt <stephen@sherra.tt>
2018-03-20 11:29:34 +0000

    Add wasm setup lines to test script

--- test.sh
+++ test.sh
@@ -8,5 +8,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cargo test
 
+rustup target add wasm32-unknown-unknown
+cargo install --git https://github.com/alexcrichton/wasm-gc
+
 $DIR/examples/tetris/wasm/build.sh --with-npm-install
 $DIR/examples/title/wasm/build.sh --with-npm-install
