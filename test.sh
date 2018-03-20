9c100a9434637ff735058279d04d0d47eebeda41
Stephen Sherratt <sfsherratt@gmail.com>
2018-03-20 12:23:01 +0000

    Maybe fix nodejs test

--- test.sh
+++ test.sh
@@ -8,8 +8,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cargo test
 
-rustup target add wasm32-unknown-unknown
-cargo install --git https://github.com/alexcrichton/wasm-gc
+if [[ "$TRAVIS_RUST_VERSION" == "beta" ]] || [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+    rustup target add wasm32-unknown-unknown
+    cargo install --git https://github.com/alexcrichton/wasm-gc || true
 
-$DIR/examples/tetris/wasm/build.sh --with-npm-install
-$DIR/examples/title/wasm/build.sh --with-npm-install
+    $DIR/examples/tetris/wasm/build.sh --with-npm-install
+    $DIR/examples/title/wasm/build.sh --with-npm-install
+fi
