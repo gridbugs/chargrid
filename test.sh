bb7e7d1624d7be9d7ea980ee1f6d2274b55f1e25
Stephen Sherratt <sfsherratt@gmail.com>
2018-03-20 10:49:42 +0000

    Simplify test script

--- test.sh
+++ test.sh
@@ -6,13 +6,4 @@ export RUSTFLAGS="--deny warnings"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cargo test --manifest-path $DIR/prototty/Cargo.toml
-cargo test --manifest-path $DIR/grid/Cargo.toml
-cargo test --manifest-path $DIR/unix/Cargo.toml
-cargo test --manifest-path $DIR/wasm/Cargo.toml
-cargo test --manifest-path $DIR/glutin/Cargo.toml
-cargo test --manifest-path $DIR/common/Cargo.toml
-cargo test --manifest-path $DIR/file-storage/Cargo.toml
-
-source examples/tetris/test.sh
-source examples/title/test.sh
+cargo test
