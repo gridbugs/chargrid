1fd83004e701babff22b050dc78f4aa3ff51cf10
Stephen Sherratt <sfsherratt@gmail.com>
2019-02-27 07:27:25 +0000

    Simplify pager

--- common/src/pager.rs
+++ common/src/pager.rs
@@ -3,51 +3,19 @@ use prototty_render::*;
 use std::mem;
 use text_info::*;
 
-pub struct Pager<T: AsRef<str>> {
-    size: Size,
-    text_info: TextInfo,
-    scroll_position: usize,
-    string: T,
-}
-
-impl<T: AsRef<str>> Pager<T> {
-    pub fn new_with_text_info(string: T, size: Size, text_info: TextInfo) -> Self {
-        Self {
-            size,
-            string,
-            text_info,
-            scroll_position: 0,
-        }
-    }
-    pub fn new(string: T, size: Size) -> Self {
-        Self::new_with_text_info(string, size, TextInfo::default())
-    }
-    pub fn up(&mut self) {
-        self.scroll_position = self.scroll_position.saturating_sub(1);
-    }
-    pub fn down(&mut self, num_wrapped_lines: usize) {
-        let max_scroll = num_wrapped_lines.saturating_sub(self.size.height() as usize + 1);
-        self.scroll_position = (self.scroll_position + 1).min(max_scroll);
-    }
-}
-
-pub struct PagerView {
+struct Buffer {
     current_word: String,
     wrapped_lines: Vec<String>,
     num_lines: usize,
 }
 
-impl PagerView {
+impl Buffer {
     pub fn new() -> Self {
         Self {
             current_word: String::new(),
             wrapped_lines: vec![String::new()],
             num_lines: 0,
         }
-    }
-
-    pub fn num_wrapped_lines(&self) -> usize {
-        self.num_lines
     }
 
     fn emit_word(&mut self, mut index_of_current_line: usize, width: usize) -> usize {
@@ -106,7 +74,7 @@ impl PagerView {
         index_of_current_line
     }
 
-    fn update_buffer(&mut self, string: &str, width: usize) {
+    fn update(&mut self, string: &str, width: usize) {
         self.current_word.clear();
         self.wrapped_lines[0].clear();
         let mut index_of_current_line = 0;
@@ -135,16 +103,56 @@ impl PagerView {
         }
         self.num_lines = index_of_current_line + 1;
     }
+
+    fn into_wrapped_lines(self) -> Vec<String> {
+        self.wrapped_lines
+    }
 }
 
-impl<T: AsRef<str>> View<Pager<T>> for PagerView {
-    fn view<G: ViewGrid>(&mut self, pager: &Pager<T>, offset: Coord, depth: i32, grid: &mut G) {
-        self.update_buffer(pager.string.as_ref(), pager.size.width() as usize);
+pub struct Pager {
+    size: Size,
+    text_info: TextInfo,
+    scroll_position: usize,
+    wrapped_lines: Vec<String>,
+}
+
+impl Pager {
+    pub fn new<T: AsRef<str>>(string: T, size: Size, text_info: TextInfo) -> Self {
+        let string = string.as_ref();
+        let mut buffer = Buffer::new();
+        buffer.update(string, size.width() as usize);
+        let wrapped_lines = buffer.into_wrapped_lines();
+        Self {
+            size,
+            text_info,
+            scroll_position: 0,
+            wrapped_lines,
+        }
+    }
+    pub fn up(&mut self) {
+        self.scroll_position = self.scroll_position.saturating_sub(1);
+    }
+    pub fn down(&mut self) {
+        let max_scroll = self
+            .wrapped_lines
+            .len()
+            .saturating_sub(self.size.height() as usize + 1);
+        self.scroll_position = (self.scroll_position + 1).min(max_scroll);
+    }
+}
+
+pub struct PagerView;
+
+impl View<Pager> for PagerView {
+    fn view<G: ViewGrid>(&mut self, pager: &Pager, offset: Coord, depth: i32, grid: &mut G) {
         let start_index = pager.scroll_position;
-        let remaining_lines_to_show = self.num_lines.saturating_sub(pager.scroll_position);
+        let remaining_lines_to_show = pager
+            .wrapped_lines
+            .len()
+            .saturating_sub(pager.scroll_position);
         let num_lines_to_show = (pager.size.height() as usize).min(remaining_lines_to_show);
         let range = start_index..(start_index + num_lines_to_show);
-        for (y_offset, line) in self.wrapped_lines[range].iter().enumerate() {
+        for (y_offset, line) in pager.wrapped_lines[range].iter().enumerate() {
             for (x_offset, ch) in line.chars().enumerate() {
                 let coord = Coord::new(x_offset as i32, y_offset as i32);
                 let view_cell = pager.text_info.view_cell_info(ch);
@@ -154,8 +162,8 @@ impl<T: AsRef<str>> View<Pager<T>> for PagerView {
     }
 }
 
-impl<T: AsRef<str>> ViewSize<Pager<T>> for PagerView {
-    fn size(&mut self, pager: &Pager<T>) -> Size {
+impl ViewSize<Pager> for PagerView {
+    fn size(&mut self, pager: &Pager) -> Size {
         pager.size
     }
 }
--- unix/examples/pager.rs
+++ unix/examples/pager.rs
@@ -11,18 +11,17 @@ fn main() -> io::Result<()> {
     let mut handle = stdin.lock();
     handle.read_to_string(&mut string)?;
     let mut context = Context::new().unwrap();
-    let mut pager_view = PagerView::new();
     let text_info = TextInfo::default()
         .foreground_colour(Rgb24::new(0, 255, 0))
         .background_colour(Rgb24::new(100, 100, 100))
         .bold();
-    let mut pager = Pager::new_with_text_info(&string, context.size().unwrap(), text_info);
+    let mut pager = Pager::new(&string, context.size().unwrap(), text_info);
     loop {
-        context.render(&mut pager_view, &pager).unwrap();
+        context.render(&mut PagerView, &pager).unwrap();
         match context.wait_input().unwrap() {
             prototty_inputs::ETX => break,
             ProtottyInput::Up => pager.up(),
-            ProtottyInput::Down => pager.down(pager_view.num_wrapped_lines()),
+            ProtottyInput::Down => pager.down(),
             _ => (),
         }
     }
