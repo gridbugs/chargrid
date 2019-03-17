8ead6b11b72516a710be1a737f3d87c6eb5f92a3
Stephen Sherratt <stephen@sherra.tt>
2019-03-17 00:46:17 +0000

    Fix unix test

--- unix/examples/bold.rs
+++ unix/examples/bold.rs
@@ -7,12 +7,16 @@ use prototty_unix::*;
 struct BoldTestView;
 
 impl View<()> for BoldTestView {
-    fn view<G: ViewGrid>(&mut self, _data: &(), offset: Coord, depth: i32, grid: &mut G) {
-        StringView.view("Hello, World!", offset + Coord::new(1, 1), depth, grid);
+    fn view<G: ViewGrid, R: ViewTransformRgb24>(
+        &mut self,
+        _data: &(),
+        context: ViewContext<R>,
+        grid: &mut G,
+    ) {
+        StringView.view("Hello, World!", context.add_offset(Coord::new(1, 1)), grid);
         RichStringView::with_info(TextInfo::default().bold()).view(
             "Hello, World!",
-            offset + Coord::new(1, 3),
-            depth,
+            context.add_offset(Coord::new(1, 3)),
             grid,
         );
     }
