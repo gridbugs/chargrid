b733da29d7a578238f5ce8d4a2b82cb129c0aee5
Stephen Sherratt <stephen@sherra.tt>
2019-07-22 15:45:29 +0100

    Fix test script

--- test.sh
+++ test.sh
@@ -15,34 +15,28 @@ cargo clean
 find -name node_modules -exec rm -rf {} \; || true
 find -name package-lock.json -delete || true
 find -name dist -exec rm -rf {} \; || true
-find -name wasm_out -exec rm -rf {} \; || true
 
 pushd $DIR/examples/tetris/wasm
 npm install
-./build_wasm.sh debug
-npx webpack
+npm run build -- --mode development
 popd
 
 pushd $DIR/examples/title/wasm
 npm install
-./build_wasm.sh debug
-npx webpack
+npm run build -- --mode development
 popd
 
 pushd $DIR/examples/fib/wasm
 npm install
-./build_wasm.sh debug
-npx webpack
+npm run build -- --mode development
 popd
 
 pushd $DIR/examples/drag/wasm
 npm install
-./build_wasm.sh debug
-npx webpack
+npm run build -- --mode development
 popd
 
 pushd $DIR/examples/pager/wasm
 npm install
-./build_wasm.sh debug
-npx webpack
+npm run build -- --mode development
 popd
