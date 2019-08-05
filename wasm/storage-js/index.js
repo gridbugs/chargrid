028fba35aafdde22700b008f16797f1cff846479
Stephen Sherratt <stephen@sherra.tt>
2019-08-05 23:05:31 +0200

    Indent js 2 spaces

--- wasm/storage-js/index.js
+++ wasm/storage-js/index.js
@@ -3,24 +3,24 @@
 import localforage from 'localforage';
 
 export class JsByteStorage {
-    js_load() {
-        return this.data;
-    }
-    js_clear() {
-        this.js_store(new Uint8Array());
-    }
-    js_store(data) {
-        this.data = data;
-        return localforage.setItem(this.key, this.data);
-    }
+  js_load() {
+    return this.data;
+  }
+  js_clear() {
+    this.js_store(new Uint8Array());
+  }
+  js_store(data) {
+    this.data = data;
+    return localforage.setItem(this.key, this.data);
+  }
 }
 JsByteStorage.make_async = async function(key) {
-    let data = await localforage.getItem(key);
-    if (data === null || data.constructor !== Uint8Array) {
-        data = new Uint8Array();
-    }
-    let storage = new JsByteStorage();
-    storage.key = key;
-    storage.data = data;
-    return storage;
+  let data = await localforage.getItem(key);
+  if (data === null || data.constructor !== Uint8Array) {
+    data = new Uint8Array();
+  }
+  let storage = new JsByteStorage();
+  storage.key = key;
+  storage.data = data;
+  return storage;
 }
