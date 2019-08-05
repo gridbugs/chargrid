fc82197b4d03d5e918c5de41a8c709299157653b
Stephen Sherratt <stephen@sherra.tt>
2019-08-06 00:22:10 +0200

    Drop localforage dependency

--- wasm/storage-js/index.js
+++ wasm/storage-js/index.js
@@ -1,26 +1,35 @@
 'use strict';
 
-import localforage from 'localforage';
+function data_to_string(d) {
+  return d.join(',');
+}
+
+function data_from_string(s) {
+  return new Uint8Array(s.split(',').map(x => parseInt(x)));
+}
 
 export class JsByteStorage {
+  constructor(key) {
+    let data_string = localStorage.getItem(key);
+    let data;
+    if (data === null) {
+      data = new Uint8Array();
+    } else {
+      data = data_from_string(data_string);
+    }
+    this.key = key;
+    this.data = data;
+
+  }
   js_load() {
     return this.data;
   }
   js_clear() {
-    this.js_store(new Uint8Array());
+    this.js_store(new Uint8Array())
   }
   js_store(data) {
     this.data = data;
-    return localforage.setItem(this.key, this.data);
+    let data_string = data_to_string(this.data);
+    localStorage.setItem(this.key, data_string);
   }
-}
-JsByteStorage.make_async = async function(key) {
-  let data = await localforage.getItem(key);
-  if (data === null || data.constructor !== Uint8Array) {
-    data = new Uint8Array();
-  }
-  let storage = new JsByteStorage();
-  storage.key = key;
-  storage.data = data;
-  return storage;
 }
