06dc11f483a8b7e55676f66eb3eecbcbf842361d
Stephen Sherratt <stephen@sherra.tt>
2019-11-26 14:22:09 +1100

    Remove unused include

--- prototty/src/lib.rs
+++ prototty/src/lib.rs
@@ -4,7 +4,5 @@ pub use prototty_event_routine as event_routine;
 pub use prototty_input as input;
 pub use prototty_menu as menu;
 pub use prototty_render as render;
-#[cfg(feature = "storage")]
-pub use prototty_storage as storage;
 pub use prototty_text as text;
 pub use render::{Coord, Size};
