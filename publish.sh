857c69fe52088c54edb0a601b82a229022be6419
Stephen Sherratt <stephen@sherra.tt>
2019-11-26 14:22:23 +1100

    Reorder publish script

--- publish.sh
+++ publish.sh
@@ -9,21 +9,24 @@ publish_single() {
 
 publish() {
     publish_single render
+    publish_single test-grid
     publish_single input
-    publish_single storage
     publish_single text
     publish_single decorator
     publish_single menu
-    publish_single prototty
-    publish_single file-storage
-    publish_single grid
-    publish_single test-grid
     publish_single event-routine
-    publish_single ansi-terminal
-    publish_single graphical
-    publish_single web
+    publish_single app
+    publish_single prototty
     publish_single audio
     publish_single native-audio
+    publish_single storage
+    publish_single file-storage
+    publish_single web
+    publish_single ansi-terminal
+    publish_single graphical
+    publish_single graphical_common
+    publish_single graphical_gfx
+
 }
 
 read -r -p "Are you sure? " response
