a8fa7fbfd7e69efb2f0f6f8ef21f1a3e436acaa5
Stephen Sherratt <stephen@sherra.tt>
2019-11-27 15:15:41 +1100

    Try running all audio on seperate thread

--- native-audio/src/lib.rs
+++ native-audio/src/lib.rs
@@ -1,29 +1,44 @@
 use prototty_audio::{AudioPlayer, AudioProperties};
 use rodio::source::Source;
-use rodio::{Decoder, Device, Sink};
+use rodio::{Decoder, Sink};
 use std::io::Cursor;
+use std::sync::mpsc;
+use std::thread;
 
 pub struct NativeAudioPlayer {
-    device: Device,
+    _audio_thread: thread::JoinHandle<()>,
+    sender: mpsc::Sender<(NativeSound, AudioProperties)>,
 }
 
 #[derive(Debug)]
 pub enum Error {
     NoOutputDevice,
-    PanicDuringInit(Box<dyn std::any::Any + Send + 'static>),
 }
 
 impl NativeAudioPlayer {
     pub fn try_new_default_device() -> Result<Self, Error> {
-        // Initialise audio on a separate thread, to prevent issue on windows
+        let (sender, receiver): (_, mpsc::Receiver<(NativeSound, AudioProperties)>) = mpsc::channel();
+        let (init_sender, init_receiver) = mpsc::channel();
+        // Run audio on a separate thread, to prevent issue on windows
         // when also using winit to create windows.
         // More info: https://github.com/RustAudio/cpal/pull/348
-        let init_thread = std::thread::spawn(|| rodio::default_output_device());
-        match init_thread.join() {
-            Ok(Some(device)) => Ok(Self { device }),
-            Ok(None) => Err(Error::NoOutputDevice),
-            Err(e) => Err(Error::PanicDuringInit(e)),
-        }
+        let _audio_thread = std::thread::spawn(move || {
+            if let Some(device) = rodio::default_output_device() {
+                init_sender.send(Ok(())).unwrap();
+                while let Ok((sound, properties)) = receiver.recv() {
+                    let sink = Sink::new(&device);
+                    let source = Decoder::new(Cursor::new(sound.bytes))
+                        .unwrap()
+                        .amplify(properties.volume);
+                    sink.append(source);
+                    sink.detach();
+                }
+            } else {
+                init_sender.send(Err(Error::NoOutputDevice)).unwrap();
+            }
+        });
+        let () = init_receiver.recv().unwrap()?;
+        Ok(Self { _audio_thread, sender })
     }
 
     pub fn new_default_device() -> Self {
@@ -31,12 +46,9 @@ impl NativeAudioPlayer {
     }
 
     pub fn play(&self, sound: &NativeSound, properties: AudioProperties) {
-        let sink = Sink::new(&self.device);
-        let source = Decoder::new(Cursor::new(sound.bytes))
-            .unwrap()
-            .amplify(properties.volume);
-        sink.append(source);
-        sink.detach();
+        self.sender
+            .send((NativeSound { bytes: sound.bytes }, properties))
+            .unwrap();
     }
 }
 
