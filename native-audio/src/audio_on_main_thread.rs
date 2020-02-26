1d1fcd08447eb1e8466cc800b1624dd437c208b9
Stephen Sherratt <stephen@sherra.tt>
2020-02-26 16:16:59 +1100

    More code reuse in native audio library

--- native-audio/src/audio_on_main_thread.rs
+++ native-audio/src/audio_on_main_thread.rs
@@ -1,16 +1,14 @@
-use crate::common;
-use crate::Error;
-use prototty_audio::{AudioPlayer, AudioProperties};
-use rodio::source::Source;
-use rodio::{Decoder, Device, Sink};
-use std::io::Cursor;
+use crate::{output_device, play_bytes_background, Error};
+use prototty_audio::AudioProperties;
+use rodio::Device;
 
 pub struct NativeAudioPlayer {
     device: Device,
 }
+
 impl NativeAudioPlayer {
     pub fn try_new_default_device() -> Result<Self, Error> {
-        let device = common::output_device().ok_or(Error::NoOutputDevice)?;
+        let device = output_device().ok_or(Error::NoOutputDevice)?;
         Ok(Self { device })
     }
 
@@ -18,33 +16,7 @@ impl NativeAudioPlayer {
         Self::try_new_default_device().unwrap()
     }
 
-    pub fn play(&self, sound: &NativeSound, properties: AudioProperties) {
-        let sink = Sink::new(&self.device);
-        let source = Decoder::new(Cursor::new(sound.bytes))
-            .unwrap()
-            .amplify(properties.volume);
-        sink.append(source);
-        sink.detach();
-    }
-}
-
-#[derive(Clone)]
-pub struct NativeSound {
-    bytes: &'static [u8],
-}
-
-impl NativeSound {
-    pub fn new(bytes: &'static [u8]) -> Self {
-        Self { bytes }
-    }
-}
-
-impl AudioPlayer for NativeAudioPlayer {
-    type Sound = NativeSound;
-    fn play(&self, sound: &Self::Sound, properties: AudioProperties) {
-        NativeAudioPlayer::play(self, sound, properties)
-    }
-    fn load_sound(&self, bytes: &'static [u8]) -> Self::Sound {
-        NativeSound::new(bytes)
+    pub fn play_bytes(&self, bytes: &'static [u8], properties: AudioProperties) {
+        play_bytes_background(&self.device, bytes, properties);
     }
 }
--- native-audio/src/lib.rs
+++ native-audio/src/lib.rs
@@ -1,9 +1,31 @@
+use prototty_audio::{AudioPlayer, AudioProperties};
+use rodio::{Decoder, Device, DeviceTrait, Sink, Source};
+use std::io::Cursor;
+
+pub(crate) fn output_device() -> Option<Device> {
+    let maybe_device = rodio::default_output_device();
+    if let Some(device) = maybe_device.as_ref() {
+        match device.name() {
+            Ok(device_name) => log::info!("Using audio device: {}", device_name),
+            Err(error) => log::warn!("Using audio device with unknown name (error: {})", error),
+        }
+    } else {
+        log::warn!("Unable to find audio device. Audio will be disabled.");
+    }
+    maybe_device
+}
+
+pub(crate) fn play_bytes_background(device: &Device, bytes: &'static [u8], properties: AudioProperties) {
+    let sink = Sink::new(device);
+    let source = Decoder::new(Cursor::new(bytes)).unwrap().amplify(properties.volume);
+    sink.append(source);
+    sink.detach();
+}
+
 #[derive(Debug)]
 pub enum Error {
     NoOutputDevice,
 }
-
-mod common;
 
 #[cfg(not(any(target_os = "windows", feature = "force_dedicated_audio_thread")))]
 mod audio_on_main_thread;
@@ -15,4 +37,25 @@ mod dedicated_audio_thread;
 #[cfg(any(target_os = "windows", feature = "force_dedicated_audio_thread"))]
 use dedicated_audio_thread as platform_specific;
 
-pub use platform_specific::{NativeAudioPlayer, NativeSound};
+pub use platform_specific::NativeAudioPlayer;
+
+#[derive(Clone)]
+pub struct NativeSound {
+    bytes: &'static [u8],
+}
+
+impl NativeSound {
+    pub fn new(bytes: &'static [u8]) -> Self {
+        Self { bytes }
+    }
+}
+
+impl AudioPlayer for NativeAudioPlayer {
+    type Sound = NativeSound;
+    fn play(&self, sound: &Self::Sound, properties: AudioProperties) {
+        NativeAudioPlayer::play_bytes(self, sound.bytes, properties)
+    }
+    fn load_sound(&self, bytes: &'static [u8]) -> Self::Sound {
+        NativeSound::new(bytes)
+    }
+}
