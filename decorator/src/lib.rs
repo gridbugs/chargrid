a46f9e6a24b9db74b4039b1a28b856981f890c19
Stephen Sherratt <stephen@sherra.tt>
2020-03-04 23:41:03 +1100

    Add pad decorator

--- decorator/src/lib.rs
+++ decorator/src/lib.rs
@@ -3,6 +3,7 @@ mod border;
 mod bound;
 mod col_modify;
 mod fill_background;
+mod pad;
 mod vertical_scroll;
 
 pub use align::*;
@@ -10,4 +11,5 @@ pub use border::*;
 pub use bound::*;
 pub use col_modify::*;
 pub use fill_background::*;
+pub use pad::*;
 pub use vertical_scroll::*;
