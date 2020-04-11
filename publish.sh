e5437a83a0b6d6aceb24a664156f7b9796c0a6ab
Stephen Sherratt <stephen@sherra.tt>
2020-04-11 18:39:17 +1000

    Remove audio from publish script

--- publish.sh
+++ publish.sh
@@ -17,7 +17,6 @@ publish() {
     publish_single event-routine
     publish_single menu
     publish_single prototty
-    publish_single audio
     publish_single web
     publish_single ansi-terminal
     publish_single graphical-common
