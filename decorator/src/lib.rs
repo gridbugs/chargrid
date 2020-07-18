07783a43dd73580b1592ed97217a22f4def5111f
Stephen Sherratt <stephen@sherra.tt>
2020-07-18 16:29:10 +1000

    Add MinSizeView decorator

--- decorator/src/lib.rs
+++ decorator/src/lib.rs
@@ -3,6 +3,7 @@ mod border;
 mod bound;
 mod col_modify;
 mod fill_background;
+mod min_size;
 mod pad;
 mod vertical_scroll;
 
@@ -11,5 +12,6 @@ pub use border::*;
 pub use bound::*;
 pub use col_modify::*;
 pub use fill_background::*;
+pub use min_size::*;
 pub use pad::*;
 pub use vertical_scroll::*;
