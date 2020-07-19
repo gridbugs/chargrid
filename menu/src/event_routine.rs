3268f0f7912fc4f03e97a555b161c856dd074128
Stephen Sherratt <stephen@sherra.tt>
2020-07-19 22:01:17 +1000

    Simplify menu EventRoutine

--- menu/src/event_routine.rs
+++ menu/src/event_routine.rs
@@ -1,104 +1,36 @@
-use crate::{MenuIndexFromScreenCoord, MenuInstance, MenuInstanceChoose};
-use chargrid_event_routine::{event_or_peek_with_handled, EventOrPeek, EventRoutine, Handled, ViewSelector};
+use crate::{MenuIndexFromScreenCoord, MenuInstanceChoose};
+use chargrid_event_routine::{
+    event_or_peek_with_handled, DataSelector, EventOrPeek, EventRoutine, Handled, ViewSelector,
+};
 use chargrid_input::Input;
 use chargrid_render::{ColModify, Frame, View, ViewContext};
-use std::marker::PhantomData;
 
-#[derive(Default)]
-pub struct MenuInstanceRoutine<V, C> {
-    view: PhantomData<V>,
-    choose: PhantomData<C>,
-}
-impl<V, C> MenuInstanceRoutine<V, C>
-where
-    C: MenuInstanceChoose,
-    for<'a> V: View<&'a MenuInstance<C::Entry>>,
-    V: MenuIndexFromScreenCoord,
-{
-    pub fn new() -> Self {
-        Self {
-            view: PhantomData,
-            choose: PhantomData,
-        }
-    }
-}
-impl<V, C> Clone for MenuInstanceRoutine<V, C>
-where
-    C: MenuInstanceChoose,
-    for<'a> V: View<&'a MenuInstance<C::Entry>>,
-    V: MenuIndexFromScreenCoord,
-{
-    fn clone(&self) -> Self {
-        Self {
-            view: PhantomData,
-            choose: PhantomData,
-        }
-    }
+pub trait ChooseSelector: DataSelector {
+    type ChooseOutput: MenuInstanceChoose;
+    fn choose_mut<'a>(&self, input: &'a mut Self::DataInput) -> &'a mut Self::ChooseOutput;
 }
 
-impl<V, C> EventRoutine for MenuInstanceRoutine<V, C>
-where
-    C: MenuInstanceChoose,
-    for<'a> V: View<&'a MenuInstance<C::Entry>>,
-    V: MenuIndexFromScreenCoord,
-{
-    type Return = C::Output;
-    type Data = C;
-    type View = V;
-    type Event = Input;
-
-    fn handle<EP>(self, data: &mut Self::Data, view: &Self::View, event_or_peek: EP) -> Handled<Self::Return, Self>
-    where
-        EP: EventOrPeek<Event = Self::Event>,
-    {
-        event_or_peek_with_handled(event_or_peek, self, |s, event| {
-            if let Some(menu_output) = data.choose(view, event) {
-                Handled::Return(menu_output)
-            } else {
-                Handled::Continue(s)
-            }
-        })
-    }
-
-    fn view<F, CM>(&self, data: &Self::Data, view: &mut Self::View, context: ViewContext<CM>, frame: &mut F)
-    where
-        F: Frame,
-        CM: ColModify,
-    {
-        view.view(data.menu_instance(), context, frame);
-    }
-}
-
-pub trait MenuInstanceExtraSelect {
-    type DataInput;
-    type Extra;
-    type Choose: MenuInstanceChoose;
-    fn choose<'a>(&self, input: &'a Self::DataInput) -> &'a Self::Choose;
-    fn choose_mut<'a>(&self, input: &'a mut Self::DataInput) -> &'a mut Self::Choose;
-    fn extra<'a>(&self, input: &'a Self::DataInput) -> &'a Self::Extra;
-}
-
-pub struct MenuInstanceExtraRoutine<S> {
+pub struct MenuInstanceRoutine<S> {
     s: S,
 }
-impl<S> MenuInstanceExtraRoutine<S>
+impl<S> MenuInstanceRoutine<S>
 where
-    S: MenuInstanceExtraSelect + ViewSelector,
+    S: DataSelector + ViewSelector + ChooseSelector,
     S::ViewOutput: MenuIndexFromScreenCoord,
-    for<'a> S::ViewOutput: View<(&'a MenuInstance<<S::Choose as MenuInstanceChoose>::Entry>, &'a S::Extra)>,
+    for<'a> S::ViewOutput: View<&'a S::DataOutput>,
 {
     pub fn new(s: S) -> Self {
         Self { s }
     }
 }
 
-impl<S> EventRoutine for MenuInstanceExtraRoutine<S>
+impl<S> EventRoutine for MenuInstanceRoutine<S>
 where
-    S: MenuInstanceExtraSelect + ViewSelector,
+    S: ViewSelector + ChooseSelector,
     S::ViewOutput: MenuIndexFromScreenCoord,
-    for<'a> S::ViewOutput: View<(&'a MenuInstance<<S::Choose as MenuInstanceChoose>::Entry>, &'a S::Extra)>,
+    for<'a> S::ViewOutput: View<&'a S::DataOutput>,
 {
-    type Return = <S::Choose as MenuInstanceChoose>::Output;
+    type Return = <S::ChooseOutput as MenuInstanceChoose>::Output;
     type Data = S::DataInput;
     type View = S::ViewInput;
     type Event = Input;
@@ -123,9 +55,8 @@ where
         F: Frame,
         C: ColModify,
     {
-        let menu_view = self.s.view_mut(view);
-        let menu_instance = self.s.choose(data).menu_instance();
-        let extra = self.s.extra(data);
-        menu_view.view((menu_instance, extra), context, frame)
+        let view = self.s.view_mut(view);
+        let data = self.s.data(data);
+        view.view(data, context, frame)
     }
 }
