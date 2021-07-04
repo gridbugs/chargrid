24bcfbf0827b9c28c2a10f43f9e70418bada96f2
Stephen Sherratt <stephen@sherra.tt>
2021-07-04 21:36:41 +1000

    Rendering in ggez backend

--- ggez/src/lib.rs
+++ ggez/src/lib.rs
@@ -1,18 +1,28 @@
 use chargrid_app::{App, ControlFlow};
 pub use chargrid_graphical_common::*;
 use chargrid_input::{keys, Input, KeyboardInput};
-use chargrid_render::{Buffer, Size, ViewContext};
+use chargrid_render::{Buffer, Rgb24, Size, ViewContext};
 use std::time::Instant;
 
 pub struct Context {
     config: Config,
 }
 
+struct Fonts {
+    normal: ggez::graphics::Font,
+    bold: ggez::graphics::Font,
+}
+
 struct GgezApp<A: App + 'static> {
-    config: Config,
+    fonts: Fonts,
     chargrid_app: A,
     buffer: Buffer,
     last_frame: Instant,
+    font_scale: ggez::graphics::PxScale,
+    underline_mesh: ggez::graphics::Mesh,
+    background_mesh: ggez::graphics::Mesh,
+    cell_width: f32,
+    cell_height: f32,
 }
 
 impl<A: App + 'static> ggez::event::EventHandler for GgezApp<A> {
@@ -35,15 +45,50 @@ impl<A: App + 'static> ggez::event::EventHandler for GgezApp<A> {
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         ggez::graphics::clear(ctx, [0., 0., 0., 1.0].into());
         for (coord, cell) in self.buffer.enumerate() {
-            ggez::graphics::queue_text(
-                ctx,
-                &ggez::graphics::Text::new(cell.character),
-                ggez::mint::Point2 {
-                    x: coord.x as f32 * self.config.cell_dimensions_px.width as f32,
-                    y: coord.y as f32 * self.config.cell_dimensions_px.height as f32,
-                },
-                Some(cell.foreground_colour.to_f32_rgba(1.).into()),
-            );
+            if cell.character != ' ' {
+                let mut text = ggez::graphics::Text::new(cell.character);
+                let font = if cell.bold {
+                    self.fonts.bold
+                } else {
+                    self.fonts.normal
+                };
+                text.set_font(font, self.font_scale);
+                ggez::graphics::queue_text(
+                    ctx,
+                    &text,
+                    ggez::mint::Point2 {
+                        x: coord.x as f32 * self.cell_width,
+                        y: coord.y as f32 * self.cell_height,
+                    },
+                    Some(cell.foreground_colour.to_f32_rgba(1.).into()),
+                );
+            }
+            if cell.background_colour != Rgb24::new(0, 0, 0) {
+                ggez::graphics::draw(
+                    ctx,
+                    &self.background_mesh,
+                    ggez::graphics::DrawParam::default()
+                        .dest(ggez::mint::Point2 {
+                            x: coord.x as f32 * self.cell_width,
+                            y: coord.y as f32 * self.cell_height,
+                        })
+                        .color(cell.background_colour.to_f32_rgba(1.).into()),
+                )
+                .expect("failed to draw background");
+            }
+            if cell.underline {
+                ggez::graphics::draw(
+                    ctx,
+                    &self.underline_mesh,
+                    ggez::graphics::DrawParam::default()
+                        .dest(ggez::mint::Point2 {
+                            x: coord.x as f32 * self.cell_width,
+                            y: coord.y as f32 * self.cell_height,
+                        })
+                        .color(cell.foreground_colour.to_f32_rgba(1.).into()),
+                )
+                .expect("failed to draw underline");
+            }
         }
         ggez::graphics::draw_queued_text(
             ctx,
@@ -109,26 +154,83 @@ impl Context {
             (config.window_dimensions_px.height as f64 / config.cell_dimensions_px.height) as u32,
         );
         let buffer = Buffer::new(grid_size);
-        let (ctx, events_loop) = ggez::ContextBuilder::new(config.title.as_str(), "chargrid_ggez")
-            .window_setup(ggez::conf::WindowSetup::default().title(config.title.as_str()))
-            .window_mode(
-                ggez::conf::WindowMode::default()
-                    .dimensions(
-                        config.window_dimensions_px.width as f32,
-                        config.window_dimensions_px.height as f32,
-                    )
-                    .resizable(config.resizable),
+        let (mut ctx, events_loop) =
+            ggez::ContextBuilder::new(config.title.as_str(), "chargrid_ggez")
+                .window_setup(ggez::conf::WindowSetup::default().title(config.title.as_str()))
+                .window_mode(
+                    ggez::conf::WindowMode::default()
+                        .dimensions(
+                            config.window_dimensions_px.width as f32,
+                            config.window_dimensions_px.height as f32,
+                        )
+                        .resizable(config.resizable),
+                )
+                .build()
+                .expect("failed to initialize ggez");
+        let fonts = Fonts {
+            normal: ggez::graphics::Font::new_glyph_font_bytes(&mut ctx, &config.font_bytes.normal)
+                .expect("failed to load normal font"),
+            bold: ggez::graphics::Font::new_glyph_font_bytes(&mut ctx, &config.font_bytes.bold)
+                .expect("failed to load bold font"),
+        };
+        let underline_mesh = {
+            let underline_mid_cell_ratio =
+                config.underline_top_offset_cell_ratio + config.underline_width_cell_ratio / 2.0;
+            let underline_cell_position =
+                (underline_mid_cell_ratio * config.cell_dimensions_px.height) as f32;
+            let underline_width =
+                (config.underline_width_cell_ratio * config.cell_dimensions_px.height) as f32;
+            let points = [
+                ggez::mint::Point2 {
+                    x: 0.0,
+                    y: underline_cell_position,
+                },
+                ggez::mint::Point2 {
+                    x: config.cell_dimensions_px.width as f32,
+                    y: underline_cell_position,
+                },
+            ];
+            let mesh = ggez::graphics::Mesh::new_line(
+                &mut ctx,
+                &points,
+                underline_width,
+                [1., 1., 1., 1.].into(),
             )
-            .build()
-            .expect("failed to initialize ggez");
+            .expect("failed to build mesh for underline");
+            mesh
+        };
+        let background_mesh = {
+            let rect = ggez::graphics::Rect {
+                x: 0.0,
+                y: 0.0,
+                w: config.cell_dimensions_px.width as f32,
+                h: config.cell_dimensions_px.height as f32,
+            };
+            let mesh = ggez::graphics::Mesh::new_rectangle(
+                &mut ctx,
+                ggez::graphics::DrawMode::fill(),
+                rect,
+                [1., 1., 1., 1.].into(),
+            )
+            .expect("failed to build mesh for background");
+            mesh
+        };
         ggez::event::run(
             ctx,
             events_loop,
             GgezApp {
-                config,
+                fonts,
                 chargrid_app: app,
                 buffer,
                 last_frame: Instant::now(),
+                font_scale: ggez::graphics::PxScale {
+                    x: config.font_scale.width as f32,
+                    y: config.font_scale.height as f32,
+                },
+                underline_mesh,
+                background_mesh,
+                cell_width: config.cell_dimensions_px.width as f32,
+                cell_height: config.cell_dimensions_px.height as f32,
             },
         )
     }
