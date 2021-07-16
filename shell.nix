4a13d5eef3abdf064875e715ea88bc7114c39507
Stephen Sherratt <stephen@sherra.tt>
2021-07-17 04:36:29 +1000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/andersk/nixpkgs-mozilla/archive/stdenv.lib.tar.gz);
+  nixpkgs = import <nixpkgs> {
+    overlays = [ moz_overlay ];
+  };
+  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override {
+    extensions = [ "rust-src" "rust-analysis" ];}
+  );
+in
+  with nixpkgs;
+  stdenv.mkDerivation rec {
+    name = "rust";
+    buildInputs = [
+      rustup
+      ruststable
+      pkg-config
+      udev
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      vulkan-loader
+      vulkan-tools
+    ];
+
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+  }
