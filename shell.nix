158a5aa6c38fe153b700e6f14e31bb4215df4f4c
Stephen Sherratt <stephen@sherra.tt>
2021-07-19 03:34:49 +1000

    Comments in nix shell file

--- shell.nix
+++ shell.nix
@@ -2,6 +2,12 @@
 #
 # Known issues:
 # - in a pure shell, alsa-lib can't connect to pulseaudio
+# - in non-NixOS environments, there are errors when initializing audio of the form:
+#   ALSA lib dlmisc.c:337:(snd_dlobj_cache_get0) Cannot open shared library libasound_module_pcm_pulse.so
+# - in non-NixOS environments, there is an error when starting a nix-shell:
+#   * Error: Unable to use "ps" to scan for ssh-agent processes
+#   * Error: Please report to x48rph@gmail.com via http://bugs.gentoo.org
+#   ^^^ This error goes away if the `LD_LIBRARY_PATH = ...` line is removed (but that line is necessary for graphical frontends to work)
 
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/stevebob/nixpkgs-mozilla/archive/with-stdenv.lib-fix.tar.gz);
@@ -33,6 +39,9 @@ stdenv.mkDerivation rec {
     vulkan-tools
   ];
 
+  # Enable backtraces on panics
   RUST_BACKTRACE = 1;
+
+  # Without this graphical frontends can't find the GPU adapters
   LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
 }
