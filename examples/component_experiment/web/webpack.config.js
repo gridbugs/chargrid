506be87dd34931a16b97eee25cef5386fb800c35
Stephen Sherratt <stephen@sherra.tt>
2021-07-31 13:40:47 +1000

    Add revision hash to wasm artifacts in example

--- examples/component_experiment/web/webpack.config.js
+++ examples/component_experiment/web/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-module.exports = (env, argv) => {
+module.exports = async (env, argv) => {
+  const revision = (await exec('git rev-parse HEAD')).stdout.trim();
   return {
     mode: 'development',
     entry: {
@@ -12,16 +15,20 @@ module.exports = (env, argv) => {
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js',
-      webassemblyModuleFilename: "app.wasm",
+      // Various levels of caching on the web will mean that updates to wasm
+      // and js files can take a long time to become visible. Prevent this by
+      // including the revision hash of the repository in the names of these
+      // files.
+      filename: `index.${revision}.js`,
+      webassemblyModuleFilename: `app.${revision}.wasm`,
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
       }),
       new WasmPackPlugin({
-        crateDirectory: path.resolve(__dirname, "."),
-        extraArgs: "--no-typescript",
+        crateDirectory: path.resolve(__dirname, '.'),
+        extraArgs: '--no-typescript',
       }),
       // Required to work in Edge
       new webpack.ProvidePlugin({
