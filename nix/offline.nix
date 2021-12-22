970d96ce07550dbee7dedbd223578fba8aa49cb7
Stephen Sherratt <stephen@sherra.tt>
2021-12-22 20:14:49 +1100

    Add rust src to offline nix config

--- nix/offline.nix
+++ nix/offline.nix
@@ -4,10 +4,14 @@ pkgs.mkShell rec {
   packages = with pkgs; [
     rustc
     cargo
+    rustPlatform.rustLibSrc
     rust-analyzer
     cargo-watch
     rustfmt
   ] ++ (import ./common.nix).projectDeps;
+
+  # Allows rust-analyzer to find the rust source
+  RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
 
   # Without this graphical frontends can't find the GPU adapters
   LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
