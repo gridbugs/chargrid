b1550d3d19c0755d9b680b205f315ac2ce654d20
Stephen Sherratt <stephen@sherra.tt>
2021-12-22 14:50:03 +1100

    Remove old shaders

--- wgpu/compile-shaders.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-TARGET=--vulkan
-OPTIMIZATION=--optimization-zero
-DEBUG=--debug
-shader-translator --vertex $TARGET $OPTIMIZATION $DEBUG < src/shader.vert > src/shader.vert.spv
-shader-translator --fragment $TARGET $OPTIMIZATION $DEBUG < src/shader.frag > src/shader.frag.spv
