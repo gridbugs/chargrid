13f2915e2578d401e79a6149cf22c08ae6437b96
Stephen Sherratt <stephen@sherra.tt>
2022-03-30 10:49:08 +1100

    Ansi terminal should compile on windows

--- ansi-terminal/src/lib.rs
+++ ansi-terminal/src/lib.rs
@@ -1,9 +1,21 @@
+#[cfg(unix)]
 mod context;
+#[cfg(unix)]
 mod error;
+#[cfg(unix)]
 mod terminal;
 
-pub use self::context::*;
-pub use self::error::*;
-pub use self::terminal::col_encode;
-pub use self::terminal::ColEncode;
-pub use chargrid_input;
+#[cfg(unix)]
+mod public {
+    pub use super::context::*;
+    pub use super::error::*;
+    pub use super::terminal::col_encode;
+    pub use super::terminal::ColEncode;
+    pub use chargrid_input;
+}
+
+#[cfg(unix)]
+pub use public::*;
+
+#[cfg(not(unix))]
+const WARNING: &str = "compiling to empty crate on non-unix system";
