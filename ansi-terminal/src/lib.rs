eb843fb5a8e0b7fa7ee461772f4e7b78ba3f21a9
Stephen Sherratt <stephen@sherra.tt>
2022-09-08 00:33:40 +0200

    Experimentally remove non-unix checks

--- ansi-terminal/src/lib.rs
+++ ansi-terminal/src/lib.rs
@@ -1,21 +1,9 @@
-#[cfg(unix)]
 mod context;
-#[cfg(unix)]
 mod error;
-#[cfg(unix)]
 mod terminal;
 
-#[cfg(unix)]
-mod public {
-    pub use super::context::*;
-    pub use super::error::*;
-    pub use super::terminal::col_encode;
-    pub use super::terminal::ColEncode;
-    pub use chargrid_input;
-}
-
-#[cfg(unix)]
-pub use public::*;
-
-#[cfg(not(unix))]
-const WARNING: &str = "compiling to empty crate on non-unix system";
+pub use chargrid_input;
+pub use context::*;
+pub use error::*;
+pub use terminal::col_encode;
+pub use terminal::ColEncode;
