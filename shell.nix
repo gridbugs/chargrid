65eb1f160823567f8abdcc89ff3720352dcc71aa
Stephen Sherratt <stephen@sherra.tt>
2022-09-30 15:32:05 +1000

    Prefer nix over flake for libraries

--- shell.nix
+++ shell.nix
@@ -1,9 +1,30 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash; }
-) {
-  src =  ./.;
-}).shellNix
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell rec {
+  packages = with pkgs; [
+    rustc
+    cargo
+    rustPlatform.rustLibSrc
+    rust-analyzer
+    cargo-watch
+    rustfmt
+    pkg-config
+    udev
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    vulkan-loader
+    vulkan-tools
+    libGL
+    bzip2
+    nodejs-16_x
+    wasm-pack
+  ];
+
+  # Allows rust-analyzer to find the rust source
+  RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+  # Without this graphical frontends can't find the GPU adapters
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
+}
