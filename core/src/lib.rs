69e0741d7791e47ce02102433303f7f2c30ef07f
Stephen Sherratt <stephen@sherra.tt>
2023-06-18 12:18:34 +0700

    Add KeyboardEvent to prelude

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -762,9 +762,9 @@ pub mod prelude {
     #[cfg(feature = "gamepad")]
     pub use super::input::{GamepadButton, GamepadInput};
     pub use super::{
-        app, ctx_tint, input, input::Input, input::KeyboardInput, input::MouseButton,
-        input::MouseInput, input::ScrollDirection, Component, Coord, Ctx, Event, FrameBuffer,
-        RenderCell, Rgba32, Size, Style, Tint,
+        app, ctx_tint, input, input::Input, input::KeyboardEvent, input::KeyboardInput,
+        input::MouseButton, input::MouseInput, input::ScrollDirection, Component, Coord, Ctx,
+        Event, FrameBuffer, RenderCell, Rgba32, Size, Style, Tint,
     };
     pub use std::time::Duration;
 }
