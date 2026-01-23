62b9832cd1a013e26a69a50cbb2050914dbf53aa
Stephen Sherratt <stephen@sherra.tt>
2026-01-24 02:26:43 +1100

    Fix build errors in examples

--- examples/colour_grid/wgpu/src/main.rs
+++ examples/colour_grid/wgpu/src/main.rs
@@ -2,28 +2,40 @@ use chargrid_wgpu::*;
 use colour_grid_app::app;
 
 fn main() {
-    let context = Context::new(Config {
-        font_bytes: FontBytes {
-            normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin.ttf").to_vec(),
-            bold: include_bytes!("./fonts/PxPlus_IBM_CGA.ttf").to_vec(),
-        },
-        title: "Colour Grid".to_string(),
-        window_dimensions_px: Dimensions {
+    let (window, event_loop) = make_window_and_event_loop(
+        "Colour Grid",
+        Dimensions {
             width: 640.,
             height: 480.,
         },
-        cell_dimensions_px: Dimensions {
-            width: 14.,
-            height: 14.,
+        false,
+    );
+    let context = Context::new(
+        &window,
+        event_loop,
+        Config {
+            font_bytes: FontBytes {
+                normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin.ttf").to_vec(),
+                bold: include_bytes!("./fonts/PxPlus_IBM_CGA.ttf").to_vec(),
+            },
+            title: "Colour Grid".to_string(),
+            window_dimensions_px: Dimensions {
+                width: 640.,
+                height: 480.,
+            },
+            cell_dimensions_px: Dimensions {
+                width: 14.,
+                height: 14.,
+            },
+            font_scale: Dimensions {
+                width: 14.,
+                height: 14.,
+            },
+            underline_width_cell_ratio: 0.1,
+            underline_top_offset_cell_ratio: 0.8,
+            resizable: false,
+            force_secondary_adapter: false,
         },
-        font_scale: Dimensions {
-            width: 14.,
-            height: 14.,
-        },
-        underline_width_cell_ratio: 0.1,
-        underline_top_offset_cell_ratio: 0.8,
-        resizable: false,
-        force_secondary_adapter: false,
-    });
+    );
     context.run(app());
 }
--- examples/component_experiment/wgpu/src/main.rs
+++ examples/component_experiment/wgpu/src/main.rs
@@ -3,28 +3,40 @@ use component_experiment_app::app;
 
 fn main() {
     env_logger::init();
-    let context = Context::new(Config {
-        font_bytes: FontBytes {
-            normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin.ttf").to_vec(),
-            bold: include_bytes!("./fonts/PxPlus_IBM_CGA.ttf").to_vec(),
-        },
-        title: "Component Experiment".to_string(),
-        window_dimensions_px: Dimensions {
+    let (window, event_loop) = make_window_and_event_loop(
+        "Component Experiment",
+        Dimensions {
             width: 640.,
             height: 480.,
         },
-        cell_dimensions_px: Dimensions {
-            width: 32.,
-            height: 32.,
+        false,
+    );
+    let context = Context::new(
+        &window,
+        event_loop,
+        Config {
+            font_bytes: FontBytes {
+                normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin.ttf").to_vec(),
+                bold: include_bytes!("./fonts/PxPlus_IBM_CGA.ttf").to_vec(),
+            },
+            title: "Component Experiment".to_string(),
+            window_dimensions_px: Dimensions {
+                width: 640.,
+                height: 480.,
+            },
+            cell_dimensions_px: Dimensions {
+                width: 32.,
+                height: 32.,
+            },
+            font_scale: Dimensions {
+                width: 32.,
+                height: 32.,
+            },
+            underline_width_cell_ratio: 0.1,
+            underline_top_offset_cell_ratio: 0.8,
+            resizable: false,
+            force_secondary_adapter: false,
         },
-        font_scale: Dimensions {
-            width: 32.,
-            height: 32.,
-        },
-        underline_width_cell_ratio: 0.1,
-        underline_top_offset_cell_ratio: 0.8,
-        resizable: false,
-        force_secondary_adapter: false,
-    });
+    );
     context.run(app());
 }
